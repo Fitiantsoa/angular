2c7663ab38288c97f6debf7e209e600ab61a9511
Anthony TROCELLIER <658167+Hequin0x@users.noreply.github.com>
2022-03-07 11:10:44 +0100

    Ajout commande getById

--- src/app/service/commande.service.ts
+++ src/app/service/commande.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Commande } from "../model/commande";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommandeService {
-  static URL: string = 'http://localhost:8080/api/commande';
 
   constructor(private http: HttpClient) { }
+
+  getById(id: number): Observable<Commande> {
+    return this.http.get<Commande>(`${environment.commandeAPIEndPoint}/${id}`);
+  }
 }
