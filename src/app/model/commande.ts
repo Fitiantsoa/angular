f94ae656149f0b14543b3e11142987898a947ff7
Anthony TROCELLIER <658167+Hequin0x@users.noreply.github.com>
2022-03-08 12:21:58 +0100

    Ajout commande statut

--- src/app/model/commande.ts
+++ src/app/model/commande.ts
@@ -1,5 +1,6 @@
 import { Utilisateur } from "./utilisateur";
 import { CommandeProduit } from "./commande-produit";
+import { CommandeStatut } from "./commande-statut";
 
 export class Commande {
   private _id: number | undefined;
@@ -7,13 +8,20 @@ export class Commande {
   private _dateCreation: Date | undefined;
   private _commandeProduits: CommandeProduit[] | undefined;
   private _prixTotal: number | undefined;
+  private _statut: CommandeStatut | undefined;
 
-  constructor(id?: number, utilisateur?: Utilisateur, dateCreation?: Date, commandeProduits?: CommandeProduit[], prixTotal?: number) {
+  constructor(id?: number,
+              utilisateur?: Utilisateur,
+              dateCreation?: Date,
+              commandeProduits?: CommandeProduit[],
+              prixTotal?: number,
+              statut?: CommandeStatut) {
     this._id = id;
     this._utilisateur = utilisateur;
     this._dateCreation = dateCreation;
     this._commandeProduits = commandeProduits;
     this._prixTotal = prixTotal;
+    this._statut = statut;
   }
 
   get id(): number | undefined {
@@ -54,5 +62,13 @@ export class Commande {
 
   set prixTotal(value: number | undefined) {
     this._prixTotal = value;
+  }
+
+  get statut(): CommandeStatut | undefined {
+    return this._statut;
+  }
+
+  set statut(value: CommandeStatut | undefined) {
+    this._statut = value;
   }
 }
