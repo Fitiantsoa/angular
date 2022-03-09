2a60ad5acdb966f7767cbb023ecddabce170aedb
pauls <sargespaul@gmail.com>
2022-03-09 21:33:10 +0100

    creation component edit produit vendre

---
+++ src/app/component/vendre/edit-produit-vendre/edit-produit-vendre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-produit-vendre',
+  templateUrl: './edit-produit-vendre.component.html',
+  styleUrls: ['./edit-produit-vendre.component.css']
+})
+export class EditProduitVendreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
