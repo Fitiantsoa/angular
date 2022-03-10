aa4de5c521bd416c9b23ee11c779b879ffe3074e
pauls <sargespaul@gmail.com>
2022-03-10 11:35:29 +0100

    Bidouille Edition Suppr VENDRE

---
+++ src/app/component/vendre/create-produit-vendre/create-produit-vendre.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Produit } from 'src/app/model/produit';
+import { ProduitService } from 'src/app/sercice/produit/produit.service';
+
+@Component({
+  selector: 'app-create-produit-vendre',
+  templateUrl: './create-produit-vendre.component.html',
+  styleUrls: ['./create-produit-vendre.component.css']
+})
+export class CreateProduitVendreComponent implements OnInit {
+
+  produit: Produit = new Produit();
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private produitService: ProduitService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.getProduit();
+  }
+
+  getProduit() {
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['id']) {
+        this.produitService.get(params['id']).subscribe((result) => {
+          this.produit = result;
+        });
+      }
+    });
+  }
+
+  save() {
+    if (this.produit.id) {
+      this.produitService.update(this.produit).subscribe((ok) => {
+        this.router.navigate(['/vendre']);
+      });
+    } else {
+      this.produitService.create(this.produit).subscribe((ok) => {
+        this.router.navigate(['/vendre']);
+      });
+    }
+  }
+
+  delete(id: number) {
+    this.produitService.delete(id).subscribe((ok) => {
+      this.router.navigate(['/vendre']);
+    });
+  }
+}
